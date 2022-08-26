d75616a0ab08838fce79ae31a0170c10f9c88221
natalieaaronsson <natalie.aaronson@wustl.edu>
2022-08-26 13:32:31 -0600

    start

---
+++ js/index.js
@@ -0,0 +1,8 @@
+console.log("Your index.js file is loaded correctly!");
+/*$("#myID").on("click", function(){
+    console.log("You Clicked on My Work")
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#myWorkSection").offset().top
+    }, 400);*/
+$( '.navUl' ).hover( handlerIn, handlerOut )
+});
